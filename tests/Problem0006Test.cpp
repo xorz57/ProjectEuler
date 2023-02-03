fefdd912b8daad91f988f08173f69dcdf1ed1224
George Fotopoulos <xorz57@gmail.com>
2023-02-03 20:47:23 +0200

    Add Problem0006Test.cpp

---
+++ tests/Problem0006Test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <cmath>
+
+static unsigned int f(unsigned int n) {
+    unsigned int sum1 = 0;
+    unsigned int sum2 = 0;
+    for (unsigned int i = 1; i <= n; i++) {
+        sum1 += i * i;
+        sum2 += i;
+    }
+    return sum2 * sum2 - sum1;
+}
+
+TEST(Problem0006, Solution) {
+    EXPECT_EQ(f(10), 2'640);
+    EXPECT_EQ(f(100), 25'164'150);
+}
