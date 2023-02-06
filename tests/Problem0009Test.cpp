4198378abd23adb854ed9693c7dd724cb39efc05
George Fotopoulos <xorz57@gmail.com>
2023-02-06 21:08:37 +0200

    Add Problem0009Test.cpp

---
+++ tests/Problem0009Test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <optional>
+
+static std::optional<unsigned int> f() {
+    for (unsigned int a = 1; a <= 1'000; a++) {
+        for (unsigned int b = 1; b <= 1'000; b++) {
+            unsigned int c = 1'000 - a - b;
+            if (a * a + b * b == c * c) {
+                return {a * b * c};
+            }
+        }
+    }
+    return {};
+}
+
+TEST(Problem0009, Solution) {
+    EXPECT_EQ(f(), 31'875'000);
+}
